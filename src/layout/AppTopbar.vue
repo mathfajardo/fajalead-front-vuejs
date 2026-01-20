95f3c1ac1e8f931bc7511f372be8b64c914abcc2
mathfajardo <mathfajardo.contato@hotmail.com>
2026-01-20 17:15:58 -0300

    deploy

---
+++ src/layout/AppTopbar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { useLayout } from "@/layout/composables/layout";
+
+const { toggleMenu, toggleDarkMode, isDarkTheme } = useLayout();
+</script>
+
+<template>
+    <div class="layout-topbar">
+        <div class="layout-topbar-logo-container">
+            <button
+                class="layout-menu-button layout-topbar-action"
+                @click="toggleMenu"
+            >
+                <i class="pi pi-bars"></i>
+            </button>
+            <router-link to="/" class="layout-topbar-logo">
+                <span>FajaLead</span>
+            </router-link>
+        </div>
+
+        <div class="layout-topbar-actions">
+            <div class="layout-config-menu">
+                <button
+                    type="button"
+                    class="layout-topbar-action"
+                    @click="toggleDarkMode"
+                >
+                    <i
+                        :class="[
+                            'pi',
+                            { 'pi-moon': isDarkTheme, 'pi-sun': !isDarkTheme },
+                        ]"
+                    ></i>
+                </button>
+            </div>
+
+            <div class="layout-topbar-menu hidden lg:block">
+                <div class="layout-topbar-menu-content">
+                    <button type="button" class="layout-topbar-action">
+                        <i class="pi pi-user"></i>
+                        <span>Profile</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
